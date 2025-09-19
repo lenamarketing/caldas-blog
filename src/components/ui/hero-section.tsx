ae0ac0891cbe4be7c79c3ac7e8be2f3e66e9e9f2
lenamarketing <omaksiutenko@innovaj.com>
2025-09-19 13:10:03 +0100

    change main banner position to "object top"

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -16,8 +16,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ className }) => {
         <img 
           src={mainBanner}
           alt="Praça da Fruta das Caldas da Rainha"
-          className="w-full h-full object-cover"
-          style={{ objectPosition: '50% 80%' }}
+          className="w-full h-full object-cover object top" 
         />
         <div className="absolute inset-0 bg-gradient-to-r from-primary/70 via-primary/40 to-transparent"></div>
       </div>
